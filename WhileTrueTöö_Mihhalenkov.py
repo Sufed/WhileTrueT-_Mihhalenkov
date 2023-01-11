a851bc2921176b87967bcf83d878f3dcb5abaaf1
ArtemiMihhalenkov <120027616+Sufed@users.noreply.github.com>
2023-01-11 13:50:28 +0200

    Update WhileTrueTöö_Mihhalenkov.py

--- WhileTrueTöö_Mihhalenkov.py
+++ WhileTrueTöö_Mihhalenkov.py
@@ -1,20 +1,36 @@
-﻿from random import *
+from random import *
 #5 Вариант
 #Harjutus 1.
 число=int(input("Число: "))
 while число <= 10:
-    print("Цикл: ", число)
     число += 1
 for число in range(1,11):
     print("Цикл: ", число)
 print()
+
+while True:
+    try:
+        число = int(input("Число: "))
+        break
+    except:
+        print("On vaja arvude tüüp kasutada")
+if число <= 10:
+    число += 1
+else:
+    print("Неа, нужно от 1-го:", end=" ")
+for число in range(1,11):
+    print("Цикл: ", число)
 #Harjutus 5.
-n=int(input("Размер квадрата: "))
-for a in range(n):
+while True:
+    try:
+        n=int(input("Размер квадрата: "))
+        break
+    except:
+        print("Неправильно.")
+for a in range(n): 
     for b in range(n):
-        if a+b==n-1:
+        if a==b or a+b==n-1:
             print("X", end=" ")
         else:
             print("O", end=" ")
-print()
-#https://moodle.edu.ee/mod/assign/view.php?id=1937108
+    print()
